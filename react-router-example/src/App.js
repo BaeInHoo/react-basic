4e3c59af9cdf16caa77dee4c2276f4772b49e1f0
BaeInHoo <qodlsgh0210@naver.com>
2022-09-13 16:00:23 +0900

    JS Routing

--- react-router-example/src/App.js
+++ react-router-example/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 import React from 'react';
 
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 import Home from './pages/home';
 import Profile from './pages/profile';
 import About from './pages/about';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 import Links from './components/Links';
 import NavLinks from './components/NavLInks';
+
+const isLogin = true;
 
 function App() {
   return (
@@ -17,6 +20,10 @@ function App() {
       <Links />
       <NavLinks />
       <Switch>
+        <Route 
+          path="/login" 
+          render={() => (isLogin ? <Redirect to='/' /> : <Login />)} 
+        />
         <Route path="/profile/:id" component={Profile} />
         <Route path="/profile" component={Profile} />
         <Route path="/about" component={About} />
